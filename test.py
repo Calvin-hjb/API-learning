ff1eed57693abff58e697873ea93783785761761
Calvin-hjb <163823134+Calvin-hjb@users.noreply.github.com>
2025-06-06 22:48:54 +0800

    Add files via upload
    storing data in memory (test)

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 #get can be change into post, put, delete
-response = requests.get(BASE+"helloworld/tim/19")
+response = requests.get(BASE + "helloworld/tim")
 print(response.json()) # use .json to let the response object be somekind of information
