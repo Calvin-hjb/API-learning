e35f0b8a5ac6f273264cbf55db1a40857bd10489
Calvin-hjb <163823134+Calvin-hjb@users.noreply.github.com>
2025-06-06 23:19:10 +0800

    Add files via upload
    request argument parser (test)

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 #get can be change into post, put, delete
-response = requests.get(BASE + "helloworld/tim")
+response = requests.put(BASE + "video/1", json={"likes": 10})
 print(response.json()) # use .json to let the response object be somekind of information
