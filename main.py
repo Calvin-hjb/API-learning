884a8cafe7b51f88d9727b92cc5ee12d922577b3
Calvin-hjb <163823134+Calvin-hjb@users.noreply.github.com>
2025-06-06 23:49:27 +0800

    Add files via upload
    validating requests, and handling delete requests

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__) #default writing to Flask app
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required = True) #reqparse will return these messages when the input is not what it supposed to
+video_put_args.add_argument("views", type=int, help="Views of the video", required = True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video", required = True)
 
-class HelloWorld(Resource): # Make a class that can handle resoruce 
-    def get(self, name, test):
-        return {"name": name, "test":test} # should be written in dictionaries. the return should be jason serializeable object
+videos = {}
+
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message = "Videos id is not valid...")
+
+def abort_if_video_exist(video_id):
+    if video_id in videos:
+        abort(409, message = "Video already exist with that ID...")
+
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        return videos[video_id]
     
-    #def post(self):
-    #    return {"data": "posted"}
+    def put(self, video_id):
+        abort_if_video_exist(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201 #201 is a response for "created"
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>") # add.resource(resource, key) key is the url and / is default url
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 
 if __name__ == "__main__": # To start server and application 
--- test.py
+++ test.py
@@ -2,6 +2,21 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-#get can be change into post, put, delete
-response = requests.get(BASE + "helloworld/tim")
-print(response.json()) # use .json to let the response object be somekind of information
+
+data = [{"likes": 11658, "name": "Tim", "views": 10000},
+        {"likes": 2568, "name": "Bella", "views": 4658},
+        {"likes": 79, "name": "Jess", "views": 10000000000}
+        ]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), json=data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/0", json={})
+print(response) 
+
+input()
+
+response = requests.get(BASE + "video/2", json={})
+print(response.json()) 
